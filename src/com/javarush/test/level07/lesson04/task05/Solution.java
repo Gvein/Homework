af7cfff2c2e0e971e8791b9d2147e5e38ee152ac
Gvein <kern1702@yandex.ru>
2015-03-12 19:20:04 +0300

    it is only beginning

---
+++ src/com/javarush/test/level07/lesson04/task05/Solution.java
@@ -0,0 +1,30 @@
+package work.src.com.javarush.test.level07.lesson04.task05;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* Один большой массив и два маленьких
+1. Создать массив на 20 чисел.
+2. Ввести в него значения с клавиатуры.
+3. Создать два массива на 10 чисел каждый.
+4. Скопировать большой массив в два маленьких: половину чисел в первый маленький, вторую половину во второй маленький.
+5. Вывести второй маленький массив на экран, каждое значение выводить с новой строки.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        //Напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[]ar1 = new int[20];
+        for(int a:ar1)
+        {
+            ar1[a] = Integer.parseInt(reader.readLine());
+
+
+        }
+
+
+    }
+}
